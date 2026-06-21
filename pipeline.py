4e126175e4953355a38d2d1861e095159f473864
Yasin Saberi <yassaberi97@gmail.com>
2026-06-21 12:07:27 +0330

    fetch_raw_data method in class

--- pipeline.py
+++ pipeline.py
@@ -4,9 +4,17 @@ class APIClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def fetch_raw_data(self, symbol):
-        pass
-
+def fetch_raw_data(self, symbol):
+        full_url = f"{self.base_url}/{symbol}"
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(full_url, headers=headers)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error fetching data: {response.status_code}")
+            return None
+    
 class DataTransformer:
     def transform_raw_payload(self, raw_data):
         pass
