d438c3ca158c0f6fcbec60e02a4c8fb2e853a4e9
Yasin Saberi <yassaberi97@gmail.com>
2026-06-21 12:46:35 +0330

    simple bugs fixed

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ class APIClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
-def fetch_raw_data(self, symbol):
+    def fetch_raw_data(self, symbol):
         full_url = f"{self.base_url}/{symbol}"
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(full_url, headers=headers)
@@ -14,6 +14,11 @@ def fetch_raw_data(self, symbol):
         else:
             print(f"Error fetching data: {response.status_code}")
             return None
+        
+class DataTransformer:
+    def transform_raw_payload(self, raw_data):
+        pass
+
 if __name__ == "__main__":
     API_BASE = "https://query1.finance.yahoo.com/v8/finance/chart"
     client = APIClient (base_url=API_BASE)
@@ -21,6 +26,3 @@ if __name__ == "__main__":
     raw_payload = client.fetch_raw_data('AAPL')
     print("Success! Raw payload type:", type(raw_payload))
     print("Keys found in the response:", raw_payload.keys())
-class DataTransformer:
-    def transform_raw_payload(self, raw_data):
-        pass
